ca5fc562a58c708de5e7acb2d482a9d77b5ea2e8
Christopher Sanders <c.sander@edu.bbq.de>
2025-10-07 09:42:40 +0200

    Initial commit

---
+++ firstread.php
@@ -0,0 +1,7 @@
+<?php
+$conn = new PDO("mysql:host=localhost;dbname=company", "phpstorm", "123456");
+$sql = "SELECT * FROM employees";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+var_dump($result);
---
+++ firstupdate.php
@@ -0,0 +1,44 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $id = $_GET['id'];
+    $conn = new PDO("mysql:host=localhost;dbname=employees", "phpstorm", "123456");
+    $sql = "SELECT * FROM employees WHERE id=:id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $fname = $result['fname'];
+    $lname = $result['lname'];
+
+
+    ?>
+    <!doctype html>
+    <head>
+        <title>Update</title>
+    </head>
+    <body>
+    <form action='' method='post'>
+        <input type="text" name=“fname“ placeholder='<?= $fname ?>'>
+        <input type="text" name=“lname“ placeholder='<?= $lname ?>'>
+        <input type="hidden" name=“id“ placeholder='<?= $id ?>'>
+        <input type="submit">
+    </form>
+    </body>
+
+    <?php
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $fname= $_POST['fname'];
+    $lname= $_POST['lname'];
+    $conn = new PDO("mysql:host=localhost;dbname=employees", "phpstorm", "123456");
+    $sql = "UPDATE employees set fname=:fname , lname=:lname WHERE id=:id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":fname", $fname);
+    $stmt->bindParam(":lname", $lname);
+    $stmt->bindParam(":id", $id);
+    $stmt->execute();
+
+}
+
+
